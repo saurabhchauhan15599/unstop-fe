c0072bab00b522ed86e2c3b0cdd6bca67c87077b
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2023-12-27 13:19:45 +0530

    project setup done

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./sidebar.module.scss";
+
+const Sidebar: React.FC = () => {
+  return <div className={css.container}>
+    <section>
+      
+    </section>
+  </div>;
+};
+
+export default Sidebar;
